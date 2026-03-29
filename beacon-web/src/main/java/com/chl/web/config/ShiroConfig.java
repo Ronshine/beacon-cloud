275d211fa56b87a107b1dd0ec703ca71735e4b3c
Dragon <2450306205@qq.com>
2026-03-29 17:12:33 +0800

    shiro配置类

---
+++ beacon-web/src/main/java/com/chl/web/config/ShiroConfig.java
@@ -0,0 +1,47 @@
+package com.chl.web.config;
+
+import com.chl.web.realm.ShiroRealm;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+    //1、过滤器链
+    @Bean
+    public ShiroFilterChainDefinition shiroFilterChainDefinition(){
+        //1、创建需要认证和放行资源
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap();
+
+        filterChainDefinitionMap.put("/sys/user/login","anon");
+        filterChainDefinitionMap.put("/index.html","anon");
+        filterChainDefinitionMap.put("/login.html","anon");
+        filterChainDefinitionMap.put("/logout","logout");
+        filterChainDefinitionMap.put("/**","authc");
+
+        //2、创建DefaultShiroFilterChainDefinition返回
+        DefaultShiroFilterChainDefinition shiroFilter = new DefaultShiroFilterChainDefinition();
+        shiroFilter.addPathDefinitions(filterChainDefinitionMap);
+
+        return shiroFilter;
+    }
+
+    //2、安全管理器
+    @Bean
+    public DefaultWebSecurityManager securityManager(ShiroRealm realm){
+        //1、创建securityManager 对象
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+
+        //2、设置自定义realm
+        securityManager.setRealm(realm);
+
+        //3、返回
+        return securityManager;
+    }
+
+}
