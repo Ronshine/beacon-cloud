45ea78500a50f86005762c1a59c2ab0b570abe5d
Dragon <2450306205@qq.com>
2026-03-29 18:23:09 +0800

    web启动类

--- beacon-web/src/main/java/com/chl/web/WebStarterApp.java
+++ beacon-web/src/main/java/com/chl/web/WebStarterApp.java
@@ -1,9 +1,11 @@
 package com.chl.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.chl.web.mapper")
 public class WebStarterApp {
     public static void main(String[] args) {
         SpringApplication.run(WebStarterApp.class,args);
