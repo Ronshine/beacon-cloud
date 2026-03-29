462e503b4a17d6c5f9ea9ab48bf8ef3c53615b21
Dragon <2450306205@qq.com>
2026-03-29 15:38:52 +0800

    web启动类

---
+++ beacon-web/src/main/java/com/chl/web/WebStarterApp.java
@@ -0,0 +1,11 @@
+package com.chl.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStarterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebStarterApp.class,args);
+    }
+}
