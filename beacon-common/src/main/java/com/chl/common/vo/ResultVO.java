ef7e081f2a395c2ada6b8b4877467febc1118612
Dragon <2450306205@qq.com>
2026-04-08 17:28:33 +0800

    新增数据属性

--- beacon-common/src/main/java/com/chl/common/vo/ResultVO.java
+++ beacon-common/src/main/java/com/chl/common/vo/ResultVO.java
@@ -5,12 +5,20 @@ public class ResultVO {
 
     private String msg;
 
+    private Object data;
+
     public ResultVO() {
     }
 
     public ResultVO(Integer code, String msg) {
         this.code = code;
         this.msg = msg;
+    }
+
+    public ResultVO(Integer code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
     }
 
     public Integer getCode() {
@@ -29,11 +37,20 @@ public class ResultVO {
         this.msg = msg;
     }
 
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
     @Override
     public String toString() {
         return "ResultVO{" +
                 "code=" + code +
                 ", msg='" + msg + '\'' +
+                ", data=" + data +
                 '}';
     }
 }
