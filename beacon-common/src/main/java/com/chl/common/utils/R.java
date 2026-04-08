58f005504335af74ec38fe25aab9315fbbb1dace
Dragon <2450306205@qq.com>
2026-04-08 17:28:16 +0800

    新增含数据的正确返回

--- beacon-common/src/main/java/com/chl/common/utils/R.java
+++ beacon-common/src/main/java/com/chl/common/utils/R.java
@@ -3,16 +3,26 @@ package com.chl.common.utils;
 import com.chl.common.enums.ExceptionEnum;
 import com.chl.common.vo.ResultVO;
 
+import java.util.Map;
+
 /**
  * 后端返回前端响应信息工具
  */
 public class R {
     /**
-     * 正确响应
+     * 正确响应,无数据
      * @return ResultVO响应对象
      */
     public static ResultVO ok(){
         return new ResultVO(0,"");
+    }
+
+    /**
+     * 正确响应,有数据
+     * @return ResultVO响应对象
+     */
+    public static ResultVO ok(Map<String,Object> map){
+        return new ResultVO(0,"", map);
     }
 
     /**
